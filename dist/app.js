858b97f05c1878c19d215b811fab3de5058fd8ea
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-06-09 16:36:04 +0600

    14-11 Error Handling in Express: 404s & Global Error Handler

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,6 +24,13 @@ app.use("/users", usersRouter);
 // app.get("/", (req: Request, res: Response) => {
 //   res.send("Welcome to ToDos App");
 // });
+// ::::: params, query ::::
+// app.get("/todos/:title/:body", (req: Request, res: Response) => {
+//   console.log("From query", req.query)
+//   console.log("From params", req.params)
+//   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+//   res.json(data);
+// });
 //::::: middleware function :::::
 // app.get("/",
 //   (req: Request, res: Response, next: NextFunction) => {
@@ -26,6 +42,20 @@ app.use("/users", usersRouter);
 //   }
 // );
 //::::: logger using middleware function :::::
+// app.get("/",
+//   (req: Request, res: Response, next: NextFunction) => {
+//     console.log({
+//       url: req.url,
+//       method: req.method,
+//       headers: req.headers
+//     });
+//     next();
+//   },
+//   (req: Request, res: Response) => {
+//     res.send("Welcome to ToDos App");
+//   }
+// );
+//::::: error handling :::::
 app.get("/", (req, res, next) => {
     console.log({
         url: req.url,
@@ -33,14 +63,33 @@ app.get("/", (req, res, next) => {
         headers: req.headers
     });
     next();
-}, (req, res) => {
-    res.send("Welcome to ToDos App");
+}, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // console.log(something)
+        res.send("Welcome to ToDos App");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // console.log(something)
+        res.send("Welcome to error er duniya");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+//::::: not found route middleware must be after all routes but just above global error handler :::::
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Route NOT FOUND" });
 });
-// ::::: params, query ::::
-// app.get("/todos/:title/:body", (req: Request, res: Response) => {
-//   console.log("From query", req.query)
-//   console.log("From params", req.params)
-//   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
-//   res.json(data);
-// });
+//::::: global error handler should be in the last:::::
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log(error);
+        res.status(400).json({ message: "something went wrong global error handler", error });
+    }
+});
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,17 @@ const usersRouter = express.Router();
 app.use("/todos", todosRouter);
 app.use("/users", usersRouter);
 
+
 // app.get("/", (req: Request, res: Response) => {
 //   res.send("Welcome to ToDos App");
+// });
+
+// ::::: params, query ::::
+// app.get("/todos/:title/:body", (req: Request, res: Response) => {
+//   console.log("From query", req.query)
+//   console.log("From params", req.params)
+//   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+//   res.json(data);
 // });
 
 //::::: middleware function :::::
@@ -32,9 +41,26 @@ app.use("/users", usersRouter);
 
 
 //::::: logger using middleware function :::::
-app.get("/",
+// app.get("/",
   
-  (req: Request, res: Response, next: NextFunction) => {
+//   (req: Request, res: Response, next: NextFunction) => {
+//     console.log({
+//       url: req.url,
+//       method: req.method,
+//       headers: req.headers
+//     });
+//     next();
+//   },
+
+//   (req: Request, res: Response) => {
+//     res.send("Welcome to ToDos App");
+//   }
+// );
+
+
+
+//::::: error handling :::::
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
     console.log({
       url: req.url,
       method: req.method,
@@ -43,20 +69,45 @@ app.get("/",
     next();
   },
 
-  (req: Request, res: Response) => {
-    res.send("Welcome to ToDos App");
+  async(req: Request, res: Response, next: NextFunction) => {
+    try {
+      // console.log(something)
+      res.send("Welcome to ToDos App");
+    } catch (error) {
+        next(error)
+    }
   }
 );
 
+app.get("/error",
+  async(req: Request, res: Response, next: NextFunction) => {
+    try {
+      // console.log(something)
+      res.send("Welcome to error er duniya");
+    } catch (error) {
+        next(error)
+    }
+  },
+);
+
+
+//::::: not found route middleware must be after all routes but just above global error handler :::::
+app.use((req, res, next)=>{
+   res.status(404).json({message: "Route NOT FOUND"})
+})
+
+
+//::::: global error handler should be in the last:::::
+app.use((error: any, req: Request, res: Response, next: NextFunction )=>{
+    if(error){
+      console.log(error)
+      res.status(400).json({message: "something went wrong global error handler", error})
+    }
+});
 
 
 
-// ::::: params, query ::::
-// app.get("/todos/:title/:body", (req: Request, res: Response) => {
-//   console.log("From query", req.query)
-//   console.log("From params", req.params)
-//   const data = fs.readFileSync(filePath, { encoding: "utf-8" });
-//   res.json(data);
-// });
+
+
 
 export default app;
